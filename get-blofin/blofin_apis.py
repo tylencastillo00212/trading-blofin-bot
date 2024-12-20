d0db4ec6c2976020fdcaea651ead1f9f02cb13e1
magic-of-software <denislidev1001@gmail.com>
2024-12-20 09:43:02 -0500

    wip: update on sending order

--- get-blofin/blofin_apis.py
+++ get-blofin/blofin_apis.py
@@ -198,7 +198,7 @@ class BlofinApis:
 
     def place_order(self, data):
         request_path = '/api/v1/trade/order'
-        url = self.demo_base_url + request_path
+        url = self.base_url + request_path
         print(url)
         method = 'POST'
         body = data
